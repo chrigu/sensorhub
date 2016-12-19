4d4841e60c2806cddef2016d0cbd6349dc693e12
Christian Cueni <christian.cueni@gmail.com>
2016-12-19 16:28:39 +0100

    add lametric support

--- app.js
+++ app.js
@@ -3,16 +3,16 @@
  */
 "use strict";
 
-let express = require('express'), 
+const express = require('express'), 
     morgan = require('morgan'), 
     jsonfile = require('jsonfile'),
-    bodyParser = require('body-parser');
-
+    bodyParser = require('body-parser'),
+    request = require('request'),
+    config = require('./config.js');
 
 const FILE = './data.json';
 
 let app = express();
-
 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
@@ -28,8 +28,10 @@ app.get('/api', function (req, res) {
 app.post('/api', function (req, res) {
     let data = {};
     if ('temperature' in req.body) {
-        data['temperature'] = req.body.temperature/100;
+        data['temperature'] = +req.body.temperature/100;
     }
+
+    updateActions(data, config)
 
     res.status(200).send("cheers");
     jsonfile.writeFile(FILE, data, function (err) {
@@ -40,3 +42,48 @@ app.post('/api', function (req, res) {
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
 })
+
+function updateActions(data, config) {
+
+    if (config.UPDATE_REMOTE) {
+        updateRemote(data, config);
+    }
+
+    if (config.UPDATE_LAMETRIC) {
+        updateLametric(data, config);
+    }
+}
+
+function updateRemote(data, config) {
+    let uri = `${config.REMOTE_PROTOCOL}://${config.REMOTE_HOST}${config.REMOTE_PATH}?temp=${data.temperature}`
+    console.log(data, uri);
+    request.get(uri);
+}
+
+function updateLametric(data, config) {
+
+    let headers = {
+        'Accept': 'application/json',
+        'X-Access-Token': config.LAMETRIC_KEY,
+        'Cache-Control': 'no-cache'
+    };
+
+    let bodyData = {
+        frames: [
+            {
+                text: `Temp: ${data.temperature}`,
+                icon: 'i3253',
+                index: 0
+            }
+        ]
+    };
+
+    request({
+            headers: headers,
+            uri: config.LAMETRIC_URL,
+            json: bodyData,
+            method: 'POST'
+        }, (err, res, body) => {
+        console.error(err)
+    });
+}
---
+++ config.sample.js
@@ -0,0 +1,14 @@
+//const
+
+var config = {
+    UPDATE_REMOTE: false,
+    REMOTE_PROTOCOL: 'http',
+    REMOTE_HOST: 'some.server.com',
+    REMOTE_PATH: '/someapi',
+    REMOTE_PORT: 443,
+    UPDATE_LAMETRIC: false,
+    LAMETRIC_URL: "some.lametric.com",
+    LAMETRIC_KEY: "someky"
+};
+
+module.exports = config;
